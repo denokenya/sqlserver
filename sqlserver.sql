7611182d091ccf81df7782daf4d0f896f62ab48c
denokenya <dennismuteti25@gmail.com>
2022-04-12 15:38:07 +0300

    add datepart

--- sqlserver.sql
+++ sqlserver.sql
@@ -4,3 +4,14 @@ SELECT GetDate()  as TodayDate;
 --Find out yesterday date
 
 SELECT GetDate() - 1 as YesterDayDate ;
+
+--DateDiff()
+--Returns the difference between two dates
+
+SELECT DateDiff(mm,'2009/10/23','2011/11/27')  ;
+
+--What is my age?
+SELECT DateDiff(YY,'1991/10/30',GetDate()) AS DIFFINYRS ;
+
+--DATE PART
+SELECT DatePart(YY,GetDate()) ;
